d14a08a4c8a50d431e9fd9d9b477c6b946798b6a
zphtry <zphtry@gmail.com>
2018-01-17 06:29:38 +0300

    1ST / OK

--- master-stage/DigitalProcessing/problem_1 (гармонический анализ)/harm.py
+++ master-stage/DigitalProcessing/problem_1 (гармонический анализ)/harm.py
@@ -1,87 +1,58 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-
-
-
-def f(t):
-    return np.exp(-t) * np.cos(2*np.pi*t)
-
-
-t1 = np.arange(0.0, 3.0, 0.01)
-
-ax1 = plt.subplot(311)
-ax1.plot(t1, f(t1), 'k')
-
-
-
-ax2 = plt.subplot(323)
-ax2.plot(t1, f(t1), 'r')
-
-ax2 = plt.subplot(324)
-ax2.plot(t1, f(t1), 'b')
-
-ax3 = plt.subplot(313)
-ax3.plot(t1, f(t1), 'g')
-
-# 222
-# 221
-# 212
-
-
-
-plt.show()
-
-exit()
-
 '''Цель работы'''
-# Выполнить над сигналом преобразование Фурье
+# Выполнить преобразование Фурье над сигналом
 # Построить амплитудную и частотную характеристику сигнала
 # Восстановить исходный сигнал
-A, tau = 2, .5
 
+A, tau = 2, .5
 def _func(arg):
 
   return A * np.exp(-arg / tau) if 0 <= arg.all() < t[-1] else 0
 
-# def fourier(arg):
-
-
-N  = 100
-t  =  np.linspace(0, .8, N)
+N  = 300
+t  =  np.linspace(0, .7, N)
 
 func = _func(t)
 
+ax1 = plt.subplot(221)
+ax1.plot(t, func, 'r')
+ax1.set_title('Original')
 
-# fourier transform
+# преобразование Фурье
 fou = []
-W  = np.linspace(0, 3000, N, endpoint=False)
+om  = np.linspace(0, 2000, N)
 for i in range(N):
-  re = np.trapz(np.multiply(func, np.cos(t * W[i])))
-  im = np.trapz(np.sin(t * W[i]), x=t)
-  fou.append(re + im * 1j)
+  re = np.trapz(func * np.cos(t * om[i]), x=t)
+  im = np.trapz(func * np.sin(t * om[i]), x=t)
+  fou.append(re - im * 1j)
 
 # амплитуды
-amp = list(map(lambda arg: np.abs(arg), fou))
+ax2 = plt.subplot(223)
+ax2.plot(om, np.abs(fou))
+ax2.set_title('Amplitudes')
 
 # частоты
-frq  = list(map(lambda arg: np.angle(arg), fou))
+ax3 = plt.subplot(224)
+ax3.plot(om, np.angle(fou))
+ax3.set_title('Frequencies')
 
 # восстановленная функция
 rst  = []
 for i in range(N):
-  rst.append((1 / (2 * np.pi)) * np.trapz(np.multiply(fou, np.exp(np.multiply(W, t[i] * 1j))), x=W))
+  a = np.trapz(np.real(fou) * np.cos(om * t[i]), x = om)
+  b = np.trapz(np.imag(fou) * np.sin(om * t[i]), x = om)
+  rst.append((a - b) / np.pi)
 
+ax4 = plt.subplot(222)
+ax4.plot(t, rst, 'g')
+ax4.set_title('Restored')
 
-f, axarr = plt.subplots(2, 2)
-axarr[0, 0].plot(x, y)
-axarr[0, 0].set_title('Axis [0,0]')
-axarr[0, 1].scatter(x, y)
-axarr[0, 1].set_title('Axis [0,1]')
-axarr[1, 0].plot(x, y ** 2)
-axarr[1, 0].set_title('Axis [1,0]')
-axarr[1, 1].scatter(x, y ** 2)
-axarr[1, 1].set_title('Axis [1,1]')
+# plt.plot(t, rst, 'g')
+# plt.title('Restored')
+# plt.savefig('restored.png')
+# exit()
 
-# plt.plot(W, rst)
-# plt.show()
+plt.tight_layout()
+plt.show()
