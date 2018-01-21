f595e163aa86f54168c012523c9743a77ab0bde1
zphtry <zphtry@gmail.com>
2018-01-22 01:26:29 +0300

    2nd / WIP

--- master-stage/DigitalProcessing/problem_2 (Котельников)/main_kotl.py
+++ master-stage/DigitalProcessing/problem_2 (Котельников)/main_kotl.py
@@ -18,77 +18,63 @@ om = np.linspace(0, omc, N)
 
 n = 7
 
-Fi = CDF(t, T, n)
 
-fou = _fourier(Fi, t, om)
+func = _func(t, Tc)
+sp_1 = plt.subplot(221)
+sp_1.plot(t, func, color=(0, .9, 0))
+sp_1.set_title('Заданный сигнал')
 
-# plt.plot(t, np.real(fou))
-# plt.plot(t, np.imag(fou))
+amp_func = np.abs(_fourier(func, t, om))
+sp_2 = plt.subplot(223)
+sp_2.plot(om, amp_func, color=(0, .5, 0))
+sp_2.set_title('Амплитудная характеристика')
 
+count_f = CDF(t, T, n)
+sp_3 = plt.subplot(222)
+sp_3.plot(t, count_f, color=(0, 0, .9))
+sp_3.set_title('Функция отсчётов')
 
-
-
-# x = np.zeros(N)
-# A, tau = 2, .5
-# for i in range(N):
-#   x[i] = A * np.exp(-t[i] / tau) if 0 <= t[i] < Tc else 0
-
-func = _func(t)
-plt.plot(t, _func)
-
-plt.show()
-exit()
-
-# амплитудная характеристика
-Xw = np.zeros(N)
-for i in range(N):
-  re = np.trapz(np.multiply(x, np.cos(t * om[i])), x=t)
-  im = np.trapz(np.multiply(x, np.sin(t * om[i])), x=t)
-  Xw[i] = np.sqrt(re ** 2 + im ** 2)
-# plt.plot(om, Xw)
+fou = _fourier(count_f, t, om)
+sp_4 = plt.subplot(224)
+sp_4.plot(t, np.real(fou), color=(0, 0, .5))
+sp_4.set_title('Спектральная характеристика')
 
 
-
-maxXw = .05 * max(Xw)
-for i in range(N):
-   if Xw[i] <= maxXw:
-       omv = om[i]
-
-print(omv)
-
-
-T = np.pi / omv
-NN = int(np.fix(Tc / T))
-t1 = np.arange(0, Tc, T / 900)
-
-
-x1 = np.zeros(len(t1))
-
-for i in range(len(t1)):
-  x1[i] = A * np.exp(-t1[i] / tau) if 0 <= t1[i] < Tc else 0
-
-# plt.plot(t1, x1)
-plt.plot(t1, x1)
+plt.tight_layout()
 plt.show()
 
-Xn1 = np.zeros(len(t1))
-for i in range(len(t1)):
+max_amp = .05 * max(amp_func)
+for i in range(N):
+  if amp_func[i] <= max_amp:
+    freq = om[i]
+
+print(freq)
+
+
+T_disc = np.pi / freq
+NN = int(np.fix(Tc / T_disc))
+# t1 = np.linspace(0, omc, N)
+# t1 = np.arange(0, Tc, T / 900)
+
+# Xn1 = np.zeros(len(t1))
+Xn1 = np.zeros(N)
+
+for i in range(N):
   for nn in range(NN + 1):
-        Xn1[i] += _func(T * (nn - 1), Tc) * CDF(t1[i], T, nn - 1)
+    Xn1[i] += _func(T * (nn - 1), Tc) * CDF(t[i], T, nn - 1)
 
+# for i in range(len(t1)):
+#   for nn in range(NN + 1):
+#     Xn1[i] += _func(T * (nn - 1), Tc) * CDF(t1[i], T, nn - 1)
 
-plt.plot(t1, Xn1)
+plt.plot(t, Xn1)
 plt.show()
 
-# figure(5);
-# subplot(1,2,1);
 # plot(t1,x1);
-# subplot(1,2,2);
 # plot(t1,Xn1);
 
 # err = abs(x1 - Xn1);
 # MaxEps = max(err);
 # disp(MaxEps);
 
-# figure(6);
 # plot(t1,err);
