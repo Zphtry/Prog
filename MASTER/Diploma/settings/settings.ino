d2d5066265b7d833d54c7fc7b082f7135cc715f5
Andrey Valikov <avalikov@phoenixit.ru>
2018-04-11 15:57:51 +0300

    upd

--- MASTER/Diploma/settings/settings.ino
+++ MASTER/Diploma/settings/settings.ino
@@ -1,50 +1,21 @@
 
 #include <SoftwareSerial.h>
 SoftwareSerial sender(6, 7);
-SoftwareSerial reciever(10, 11);
+// SoftwareSerial reciever(10, 11);
 
 
 void setup() {
   Serial.begin(9600);
   sender.begin(9600);
-  reciever.begin(9600);
-
-  delay(2000);
-
-  sender.write("AT");
-  reciever.write("AT");
-
-  delay(2000);
-
-  Serial.write("Sender: ");
-  Serial.write(sender.read());
- // while (sender.available() > 0) {
-  //}
-
-  Serial.write("Reciever: ");
-  Serial.write(reciever.read());
-  //while (reciever.available() > 0) {
-  //}
-
-  Serial.println("");
-
-
-  //reciever.write("AT+RX");
-//
-  //delay(1000);
-  //Serial.write("Reciever: ");
-  //while (reciever.available() > 0) {
-  //  Serial.write(reciever.read());
-  //}
-
-
-
+  pinMode(D1, INPUT_PILLDOWN);
 }
 
 
 void loop() {
+  sender.write("test");
 
-
+  delay(2000);
+  Serial.write(sender.read());
 }
 
 
