7a3c520eab3cd423e00b3ac7bd34047b10ce70fb
Andrey Valikov <avalikov@phoenixit.ru>
2018-04-14 02:28:15 +0300

    [diploma] backend lil success

--- MASTER/Diploma/settings/settings.ino
+++ MASTER/Diploma/settings/settings.ino
@@ -1,29 +1,80 @@
 
 #include <SoftwareSerial.h>
 SoftwareSerial sender(8, 9);
+int setPin = 7;
+
+String command = "";
 
 
 void setup() {
+
   Serial.begin(9600);
   sender.begin(9600);
-  pinMode(7, OUTPUT); 
-//  reciever.begin(9600);
 
+  pinMode(setPin, OUTPUT);
+  digitalWrite(setPin, HIGH);
 }
+
 
 
 void loop() {
-    digitalWrite(7, HIGH);
-  
-    sender.write("AT");
 
-    while (sender.available() > 0) {
-      Serial.write(sender.read());
-    }
+  command = readSerial();
+
+  if (command == "config=true") {
+    enableConfig();
+  }
+
+  else
+
+  if (command == "config=false") {
+    disable_config();
+  }
+
+  else {
+    sender.print(command);
+  }
+
+
+
+  Serial.print(readSender());
+
 
   delay(500);
 
+  command = "";
 }
+
+void enableConfig() {
+  digitalWrite(setPin, LOW);
+}
+
+void disable_config() {
+  digitalWrite(setPin, HIGH);
+}
+
+
+
+String readSerial() {
+  String command = "";
+
+  while (Serial.available() > 0) {
+    command += char(Serial.read());
+  }
+
+  return command;
+}
+
+String readSender() {
+  String command = "";
+
+  while (sender.available() > 0) {
+    command += char(sender.read());
+  }
+
+  return command;
+}
+
 
 
 
