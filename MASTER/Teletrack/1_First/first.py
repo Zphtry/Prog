c9b9d19940de04f47f706b20afd8179537a5f15a
Andrey Valikov <avalikov@phoenixit.ru>
2018-05-30 00:20:48 +0300

    many many lines

--- MASTER/Teletrack/1_First/first.py
+++ MASTER/Teletrack/1_First/first.py
@@ -1,86 +1,86 @@
+import math
+import matplotlib.pyplot as plt
+
+fact = math.factorial
+
 # начальные значения и переменные
- 
-l_left = .01
-l_right = 1
+l_left, l_right = .01, 1
 delta_l = .05
-M = 10
-N = 10
-m = .9
+M, N, m = 10, 10, .9
 
 # 1
-X1 = factorial(M) / (factorial(N) * factorial(M-N))
+X1 = fact(M) / (fact(N) * fact(M - N))
 k = 0
 X = l_left
 
+P1_k, G1_k, E1_k = [], [], []
+
 while X < l_right:
-    X2 = (X / m)^N
-    S = 0
-    Y = 0
+    X2 = (X / m) ** N
+    S, Y = 0, 0
     while Y <= N:
-        Y2 = (X / m) ^ Y
-        Y1 = factorial(M)/(factorial(Y)*factorial(M-Y))
-        S = S + Y1*Y2
-        Y = Y + 1
-    P1_k[k + 1] = X1 * (X2 / S)
-    G1_k[k + 1] = X* M * (1 - P1_k[k+1])
-    E1_k[k + 1] = (X * M / m) * (1 - P1_k[k+1])
+        Y2 = (X / m) ** Y
+        Y1 = fact(M) / (fact(Y) * fact(M - Y))
+        S += Y1 * Y2
+        Y += 1
+    P1_k.append(X1 * (X2 / S))
+    G1_k.append(X * M * (1 - P1_k[-1]))
+    E1_k.append((X * M / m) * (1 - P1_k[-1]))
+
     XX_k = X
+    X += delta_l
+
+# 2
+X = l_left
+P2_k, G2_k, E2_k = [], [], []
+
+while X < l_right:
+    X2 = ((X * M / m) ** N) / fact(N)
+    S, Y = 0, 0
+    while Y <= (N + 1):
+        Y2 = ((X * M / m) ** Y) / fact(Y)
+        S += Y2
+        Y += 1
+    P2_k.append(X2 / S)
+    G2_k.append(X * M * (1 - P2_k[-1]))
+    E2_k.append((X * M / m) * (1 - P2_k[-1]))
     X += delta_l
     k += 1
 
+# 3
+P3_k, G3_k, E3_k = [], [], []
 
-# 2
-k = 0
+X1 = fact(M) / (fact(N) * fact(M - N))
 X = l_left
- 
+
 while X < l_right:
-    X2 = ((X*M/m)^N)/factorial(N);
-    S = 0;
-    Y = 0;
-    while(Y <= (N+1))
-        Y2 = ((X*M/m)^Y)/factorial(Y);
-        S = S + Y2;
-        Y = Y + 1;
-    end
-    P2_k(k+1) = X2/S;
-    G2_k(k+1) = X*M*(1 - P2_k(k+1));
-    E2_k(k+1) = (X*M/m)*(1-P2_k(k+1));
-    X = X + delta_l;
-    k = k + 1;
-end
- 
-%3
-X1 = factorial(M)/(factorial(N)*factorial(M-N));
-k = 0;
-X = l_left;
- 
-while(X < l_right)
-    X2 = (X/(m+X))^N;
-    Y2 = (1-(X/(m+X)))^(M-N);
-    P3_k(k+1) = X1*X2*Y2;
-    G3_k(k+1) = X*M*(1 - P3_k(k+1));
-    E3_k(k+1) = (X*M/m)*(1-P3_k(k+1));
-    X = X + delta_l;
-    k = k + 1;
-end
- 
-figure(1);
-plot(l_left:delta_l:l_right, P1_k, l_left:delta_l:l_right, P2_k, l_left:delta_l:l_right, P3_k);
-grid on;
-ylabel('Вероятность потерь вызовов');
-xlabel('Lambda');
-legend('Эрланг', 'Энгсет', 'Биноминальное распределение')
- 
-figure(2);
-plot(l_left:delta_l:l_right, G1_k, l_left:delta_l:l_right, G2_k, l_left:delta_l:l_right, G3_k);
-grid on;
-ylabel('Производительность');
-xlabel('Lambda');
-legend('Эрланг', 'Энгсет', 'Биноминальное распределение')
- 
-figure(3);
-plot(l_left:delta_l:l_right, G1_k, l_left:delta_l:l_right, G2_k, l_left:delta_l:l_right, G3_k);
-grid on;
-ylabel('Среднее число соединений');
-xlabel('Lambda');
-legend('Эрланг', 'Энгсет', 'Биноминальное распределение')
+    X2 = (X / (m + X)) ** N
+    Y2 = (1 - (X / (m + X))) ** (M - N)
+    P3_k.append(X1 * X2 * Y2)
+    G3_k.append(X * M * (1 - P3_k[-1]))
+    E3_k.append((X * M / m) * (1 - P3_k[-1]))
+    X += delta_l
+
+# ylabel('Вероятность потерь вызовов');
+# xlabel('Lambda');
+# legend('Эрланг', 'Энгсет', 'Биноминальное распределение')
+plt.plot(P1_k)
+plt.plot(P2_k)
+plt.plot(P3_k)
+plt.show()
+
+# ylabel('Производительность');
+# xlabel('Lambda');
+# legend('Эрланг', 'Энгсет', 'Биноминальное распределение')
+plt.plot(G1_k)
+plt.plot(G2_k)
+plt.plot(G3_k)
+plt.show()
+
+# ylabel('Среднее число соединений');
+# xlabel('Lambda');
+# legend('Эрланг', 'Энгсет', 'Биноминальное распределение')
+plt.plot(E1_k)
+plt.plot(E2_k)
+plt.plot(E3_k)
+plt.show()
