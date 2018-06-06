ec29b4501480510e0982d321c6f88d4473435670
Andrey Valikov <avalikov@phoenixit.ru>
2018-06-06 03:16:54 +0300

    refactored

--- MASTER/Teletrack/1_First/first.py
+++ MASTER/Teletrack/1_First/first.py
@@ -10,12 +10,15 @@ import scipy.misc as scipy_misc
 
 fact = math.factorial
 
-# интенсивность нагрузки
+# поступление нагрузки
 # начальное значение, шаг, конечное значение
 l_left, l_step, l_right = .01, .05, 1
 
+# уход нагрузки
+mu = .9
+
 # число входов / число выходов / интенсивность ухода нагрузки
-M, N, mu = 10, 10, .9
+M, N = 10, 10
 
 comb = scipy_misc.comb(M, N)
 
@@ -26,33 +29,44 @@ P2_k, G2_k, E2_k = [], [], []
 P3_k, G3_k, E3_k = [], [], []
 
 
+def p_engset(lam):
+    _sum = sum((scipy_misc.comb(M, n) * ((lam / mu) ** n) for n in range(N + 1)))
+    return comb * ((lam / mu) ** N / _sum)
+
+
+def p_erlang(lam):
+    _sum = sum(((M * lam / mu) ** n) / fact(n) for n in range(N))
+    return (M * lam / mu) ** N / fact(N) / _sum
+
+
+def p_binom(lam):
+    alpha = lam / (mu + lam)
+    return comb * (alpha ** N) * (1 - alpha) ** (M - N)
+
+
 def perf(lam, p):
     """performance"""
     return lam * M * (1 - p)
 
 
-def cons(_l_to_mu, p):
+def cons(lam, p):
     """connections"""
-    return _l_to_mu * M * (1 - p)
+    return perf(lam, p) / mu
 
 
 for l in np.arange(l_left, l_right, l_step):
-    l_to_mu = l / mu
 
-    part_sum = sum((scipy_misc.comb(M, n) * (l_to_mu ** n) for n in range(N + 1)))
-    P1_k.append(comb * (l_to_mu ** N / part_sum))
+    P1_k.append(p_engset(l))
     G1_k.append(perf(l, P1_k[-1]))
-    E1_k.append(cons(l_to_mu, P1_k[-1]))
+    E1_k.append(cons(l, P1_k[-1]))
 
-    part_sum = sum(((M * l_to_mu) ** n) / fact(n) for n in range(N))
-    P2_k.append(((M * l_to_mu) ** N) / fact(N) / part_sum)
+    P2_k.append(p_erlang(l))
     G2_k.append(perf(l, P2_k[-1]))
-    E2_k.append(cons(l_to_mu, P2_k[-1]))
+    E2_k.append(cons(l, P2_k[-1]))
 
-    alpha = l / (mu + l)
-    P3_k.append(comb * (alpha ** N) * (1 - alpha) ** (M - N))
+    P3_k.append(p_binom(l))
     G3_k.append(perf(l, P3_k[-1]))
-    E3_k.append(cons(l_to_mu, P3_k[-1]))
+    E3_k.append(cons(l, P3_k[-1]))
 
 # ylabel('Вероятность потерь вызовов');
 # xlabel('Lambda');
