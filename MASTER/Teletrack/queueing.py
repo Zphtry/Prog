c78dc187a35b058c552341c4fb0a1e75370606f7
Andrey Valikov <avalikov@phoenixit.ru>
2018-06-19 14:58:58 +0300

    λ and μ works

--- MASTER/Teletrack/queueing.py
+++ MASTER/Teletrack/queueing.py
@@ -1,15 +1,14 @@
 import matplotlib.pyplot as plt
 import scipy.misc as scipy_misc
 import math
+from mode_of_work import Mode
 fact = math.factorial
 
 
-class MassService:
+class Queueing:
     def __init__(self, lam, mu, M, N, _range, mode):
-        self.lam = lam
-        self.mu = mu
-        self.M = M
-        self.N = N
+        self.lam, self.mu = lam, mu
+        self.M, self.N = M, N
         self._range = _range
         self.rho = lam / mu
         self.comb = scipy_misc.comb(M, N)
@@ -17,13 +16,13 @@ class MassService:
 
     def p_engset(self):
         """engset loss probability"""
-        _sum = sum((scipy_misc.comb(self.M, n) * ((self.lam / self.mu) ** n) for n in range(self.N + 1)))
-        return self.comb * (((self.lam / self.mu) ** self.N) / _sum)
+        _sum = sum((scipy_misc.comb(self.M, n) * (self.rho ** n) for n in range(self.N + 1)))
+        return self.comb * ((self.rho ** self.N) / _sum)
 
     def p_erlang(self):
         """erlang loss probability"""
-        _sum = sum(((self.M * self.lam / self.mu) ** n) / fact(n) for n in range(self.N + 2))
-        return (self.M * self.lam / self.mu) ** self.N / (fact(self.N) * _sum)
+        _sum = sum(((self.M * self.rho) ** n) / fact(n) for n in range(self.N + 2))
+        return (self.M * self.rho) ** self.N / (fact(self.N) * _sum)
 
     def p_binom(self):
         """binom loss probability"""
@@ -39,8 +38,22 @@ class MassService:
         return self.perf(p) / self.mu
 
     def common_plot(self, **kwargs):
-        title = f"{kwargs['title']}M={self.M}, N={self.N}, mu={self.mu}"
-        file_title = (kwargs['title_eng'] + '_M{}_N{}_mu{}').format(self.M, self.N, self.mu)
+        title = f"{kwargs['title']} "
+        if self.mode == Mode.on_lambda:
+            title += f'M={self.M} N={self.N} μ={self.mu}'
+            plt.xlabel('λ')
+        elif self.mode == Mode.on_mu:
+            title += f'M={self.M} N={self.N} λ={self.lam}'
+            plt.xlabel('μ')
+        elif self.mode == Mode.on_rho:
+            title += f'M={self.M} N={self.N}'
+            plt.xlabel('ρ')
+        elif self.mode == Mode.on_k:
+            title += f'M={self.M} N={self.N} ρ={self.rho}'
+            plt.xlabel('k = M / N')
+
+    # file_title = f"_M{self.M}_N{self.N}_mu{self.mu}"
+
         plt.title(title)
         plt.plot(self._range, kwargs['engset'], label='Энгсет')
         plt.plot(self._range, kwargs['erlang'], label='Эрланг')
--- MASTER/Teletrack/test.py
+++ MASTER/Teletrack/test.py
@@ -1,24 +1,21 @@
 import math
-fact = math.factorial
-from funcs import MassService
-from mode_of_work import Mode
 import numpy as np
-import scipy.misc as scipy_misc
+from queueing import Queueing
+from mode_of_work import Mode
 
+fact = math.factorial
 
 """Режим работы (какой параметр будет меняться)"""
-mode = Mode.on_lambda
+mode = Mode.on_rho
 
 # поступление нагрузки / уход нагрузки / соотношение
-lam, mu = 0, .9; rho = lam / mu
+lam, mu = .5, .5
 
 # число входов / число выходов
 M, N = 50, 10
 
 # один из четырёх параметров, который будет меняться
 _range = np.arange(.01, 1, .05)
-
-comb = scipy_misc.comb(M, N)
 
 # вероятность потерь вызовов / производительность / среднее число соединений
 P1_k, G1_k, E1_k = [], [], []
@@ -34,31 +31,37 @@ P3_k, G3_k, E3_k = [], [], []
 # elif mode == Mode.on_k:
 #     pass
 
-mass_service = MassService(lam, mu, M, N, _range, mode)
+queueing = Queueing(lam, mu, M, N, _range, mode)
 
 for var in _range:
 
     if mode == Mode.on_lambda:
-        mass_service.lam = var
+        queueing.lam, queueing.rho = var, var / mu
     elif mode == Mode.on_mu:
-        pass
+        queueing.mu, queueing.rho = var, lam / var
     elif mode == Mode.on_rho:
-        pass
+        queueing.rho = var
     elif mode == Mode.on_k:
         pass
 
-    P1_k.append(mass_service.p_engset())
-    G1_k.append(mass_service.perf(P1_k[-1]))
-    E1_k.append(mass_service.cons(P1_k[-1]))
+    P1_k.append(queueing.p_engset())
+    G1_k.append(queueing.perf(P1_k[-1]))
+    E1_k.append(queueing.cons(P1_k[-1]))
 
-    P2_k.append(mass_service.p_erlang())
-    G2_k.append(mass_service.perf(P2_k[-1]))
-    E2_k.append(mass_service.cons(P2_k[-1]))
+    P2_k.append(queueing.p_erlang())
+    G2_k.append(queueing.perf(P2_k[-1]))
+    E2_k.append(queueing.cons(P2_k[-1]))
 
-    P3_k.append(mass_service.p_binom())
-    G3_k.append(mass_service.perf(P3_k[-1]))
-    E3_k.append(mass_service.cons(P3_k[-1]))
+    P3_k.append(queueing.p_binom())
+    G3_k.append(queueing.perf(P3_k[-1]))
+    E3_k.append(queueing.cons(P3_k[-1]))
 
-common_plot(title='Вероятность потерь вызовов', title_eng='loss_prob', engset=P1_k, erlang=P2_k, binom=P3_k)
-common_plot(title='Производительность', title_eng='perf', engset=G1_k, erlang=G2_k, binom=G3_k)
-common_plot(title='Среднее число соединений', title_eng='aver_conn', engset=E1_k, erlang=E2_k, binom=E3_k)
+queueing.common_plot(
+    title='Вероятность потерь вызовов', title_eng='loss_prob',
+    engset=P1_k, erlang=P2_k, binom=P3_k)
+queueing.common_plot(
+    title='Производительность', title_eng='perf',
+    engset=G1_k, erlang=G2_k, binom=G3_k)
+queueing.common_plot(
+    title='Среднее число соединений', title_eng='aver_conn',
+    engset=E1_k, erlang=E2_k, binom=E3_k)
