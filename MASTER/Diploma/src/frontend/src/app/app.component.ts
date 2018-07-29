88d8edd2149ba961e16335880fd1ab3abb7c6057
Andrey Valikov <zphtry@gmail.com>
2018-07-30 00:30:41 +0300

    frontend layout

--- MASTER/Diploma/src/frontend/src/app/app.component.ts
+++ MASTER/Diploma/src/frontend/src/app/app.component.ts
@@ -1,10 +1,40 @@
 import { Component } from '@angular/core';
+import {range} from 'lodash';
+
+
+enum Modes {
+  FU1 = 'FU1',
+  FU2 = 'FU2',
+  FU3 = 'FU3',
+  FU4 = 'FU4'
+}
+
+const BAUD_RATES: number[] = [
+  1200,
+  2400,
+  4800,
+  9600,
+  19200,
+  38400,
+  57600,
+  115200
+];
+
+const POWER: number[] = range(1, 8 + 1);
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  templateUrl: 'app.component.html',
+  styleUrls: ['app.component.scss']
 })
 export class AppComponent {
-  title = 'app';
+  public readonly MODES: string[] = Object.keys(Modes);
+  public readonly BAUD_RATES: number[] = BAUD_RATES;
+  public readonly POWER: number[] = POWER;
+  public config = false;
+  public mode: Modes = Modes.FU1;
+  test() {
+    console.log('test');
+    console.log(this.config);
+  }
 }
--- MASTER/Diploma/src/frontend/src/app/app.module.ts
+++ MASTER/Diploma/src/frontend/src/app/app.module.ts
@@ -2,13 +2,88 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import {
+    MatAutocompleteModule,
+    MatBadgeModule,
+    MatButtonModule,
+    MatButtonToggleModule,
+    MatCardModule,
+    MatCheckboxModule,
+    MatChipsModule,
+    MatDatepickerModule,
+    MatDialogModule,
+    MatDividerModule,
+    MatExpansionModule,
+    MatGridListModule,
+    MatIconModule,
+    MatInputModule,
+    MatListModule,
+    MatMenuModule,
+    MatNativeDateModule,
+    MatPaginatorModule,
+    MatProgressBarModule,
+    MatProgressSpinnerModule,
+    MatRadioModule,
+    MatRippleModule,
+    MatSelectModule,
+    MatSidenavModule,
+    MatSliderModule,
+    MatSlideToggleModule,
+    MatSnackBarModule, MatSortModule,
+    MatStepperModule, MatTableModule,
+    MatTabsModule,
+    MatToolbarModule,
+    MatTooltipModule,
+    MatTreeModule
+} from '@angular/material';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+
+/* IMPORTS */
+const materialImports = [
+    MatAutocompleteModule,
+    MatBadgeModule,
+    MatPaginatorModule,
+    MatButtonModule,
+    MatButtonToggleModule,
+    MatCardModule,
+    MatCheckboxModule,
+    MatChipsModule,
+    MatDatepickerModule,
+    MatDialogModule,
+    MatDividerModule,
+    MatExpansionModule,
+    MatGridListModule,
+    MatIconModule,
+    MatInputModule,
+    MatListModule,
+    MatMenuModule,
+    MatNativeDateModule,
+    MatProgressBarModule,
+    MatProgressSpinnerModule,
+    MatRadioModule,
+    MatRippleModule,
+    MatSelectModule,
+    MatSidenavModule,
+    MatSliderModule,
+    MatSlideToggleModule,
+    MatSortModule,
+    MatStepperModule,
+    MatTableModule,
+    MatTabsModule,
+    MatToolbarModule,
+    MatTooltipModule,
+    MatTreeModule,
+    MatSnackBarModule,
+];
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    ...materialImports,
+    BrowserModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
